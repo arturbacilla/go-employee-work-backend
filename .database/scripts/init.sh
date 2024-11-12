79d648a3845da8c4362ab2a99b8dd5656f9e9f55
Artur Bacilla <artur.bacilla1@gmail.com>
2024-11-12 18:15:51 -0300

    fix: Fix db healthcheck

--- .database/scripts/init.sh
+++ .database/scripts/init.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE TRYBE;
-    SELECT 'CREATE DATABASE TRYBE'
-    WHERE NOT EXISTS (SELECT FROM pg_database where datname = 'TRYBE')
+    CREATE DATABASE $POSTGRES_DB;
+    SELECT 'CREATE DATABASE $POSTGRES_DB'
+    WHERE NOT EXISTS (SELECT FROM pg_database where datname = '$POSTGRES_DB')
 EOSQL
 
